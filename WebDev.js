a6b6b021093db76b02685e456258d03a21855f5f
Raluca <raluca.constanda@gmail.com>
2013-09-18 14:45:54 +0300

    Create WebDev.js

---
+++ WebDev.js
@@ -0,0 +1,76 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Raluca
+ * Date: 9/17/13
+ * Time: 3:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function applyFunctions(i, functions) {
+    var results = [];
+    for (var j = 0; j < functions.length; j++) {
+        results[j] = functions[j](i);
+    }
+    return results;
+}
+
+function mainFunction() {
+    var selected = [];
+    var i = 0;
+    var n = parseInt(document.getElementById("no").value);
+
+    if(document.getElementById("add_one").checked){
+        selected[i] = add_one;
+        i++;
+    }
+    if(document.getElementById("multiply_by_five").checked){
+        selected[i] = multiply_by_five;
+        i++;
+    }
+    if(document.getElementById("multiply_by_two").checked){
+        selected[i] = multiply_by_two;
+        i++;
+    }
+    if(document.getElementById("substract_five").checked){
+        selected[i] = substract_five;
+        i++;
+    }
+    if(document.getElementById("substract_three").checked){
+        selected[i] = substract_three;
+        i++;
+    }
+    if(document.getElementById("add_two").checked){
+        selected[i] = add_two;
+        i++;
+    }
+    var results = applyFunctions(n, selected);
+    var html ='';
+    for(var i = 0; i < results.length; i++){
+        html += results[i];
+        if(i !== results.length - 1){
+            html += ', ';
+        }
+    }
+    $('#results').html(html);
+    $('#response').show();
+}
+
+function add_one(n){
+    var m = n + 1;
+    return m;
+}
+function multiply_by_five(n){
+    return n*5;
+}
+function multiply_by_two(n){
+    return n*2;
+}
+function substract_five(n){
+    return n-5;
+}
+function substract_three(n){
+    return n-3;
+}
+function add_two(n){
+    return n+2;
+}
